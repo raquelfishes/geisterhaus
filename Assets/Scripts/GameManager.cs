7250325f4644ae9a5aa0a1a47adfc99ba3bb4813
Julio Martin <j.martinsae@alumnos.urjc.es>
2015-02-10 18:42:18 +0100

    human player and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject[] humans;
+	public GameObject[] ghosts;
+	public GameObject[] ghost_objects;
+	private int human_selected = 0;
+
+	// Use this for initialization
+	void Start () {
+		humans = GameObject.FindGameObjectsWithTag("Human");
+		humans [human_selected].GetComponent<HumanPlayer>().select();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("ChangeH"))
+			nextSelectedHuman ();
+	}
+
+	void nextSelectedHuman(){
+		humans [human_selected].GetComponent<HumanPlayer>().deselect ();
+		human_selected = (human_selected+1)%humans.Length();
+		humans [human_selected].GetComponent<HumanPlayer>().select ();
+	}
+
+
+}
---
+++ Assets/Scripts/HumanPlayer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayer : MonoBehaviour {
+
+	private int life;
+	public Vector3 direction;
+	private bool selected=false;
+
+
+	// Use this for initialization
+	void Start () {
+		direction = new Vector3 (1.0f, 0.0f, 0.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected) {
+			float horAxis = Input.GetAxisRaw ("Horizontal");
+			if (horAxis != 0.0f)
+				turnHorizontal(horAxis);
+			float vertAxis = Input.GetAxisRaw ("Vertical");
+			if (vertAxis != 0.0f)
+				turnVertical(vertAxis);
+		}
+		transform.Translate (direction * Time.deltaTime);
+	}
+
+	private void turnHorizontal(float axis){
+		if (axis > 0.0f)
+			//Turn right
+			direction = Vector3.right;
+		else
+			//Turn left
+			direction = Vector3.left;
+	}
+
+	private void turnVertical(float axis){
+		if (axis > 0.0f)
+			//Turn forward
+			direction = Vector3.forward;
+		else
+			//Turn back
+			direction = Vector3.back;
+	}
+
+	public void setDirection(Vector3 vec){
+		direction = vec;
+	}
+
+	public Vector3 getDirection(){
+		return direction;
+	}
+
+	public void downLife(){
+		--life;
+	}
+
+	public void setLife(int li){
+		life = li;
+	}
+
+	public int getLife(){
+		return life;
+	}
+
+	public void deselect(){
+		selected = false;
+	}
+
+	public void select(){
+		selected = true;
+	}
+
+}
